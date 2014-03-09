b9f9f727ae951bd6a4525a0446f199013b23e4ca
kn65op <kn65op@gmail.com>
2014-03-09 13:19:47 +0100

    Add Thing files

---
+++ Code/depot/inc/Thing.h
@@ -0,0 +1,12 @@
+#ifndef THING_H
+#define THING_H
+
+#include <string>
+
+class Thing
+{
+private:
+    std::string name;
+};
+
+#endif // THING_H
---
+++ Test/src/depotTests/ThingTest.cpp
@@ -0,0 +1,8 @@
+#include "depot/inc/Thing.h"
+
+#include <gtest/gtest.h>        
+
+TEST(ThingTests, ThingCreate)
+{
+  Thing t;
+}
