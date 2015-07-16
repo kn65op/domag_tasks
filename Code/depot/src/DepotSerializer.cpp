74b22189d633d036672e61779a794327a53206f2
kn65op <kn65op@gmail.com>
2015-07-16 09:02:33 +0200

    Simple article test

--- Code/depot/src/DepotSerializer.cpp
+++ Code/depot/src/DepotSerializer.cpp
@@ -1,8 +1,13 @@
 #include "../inc/DepotSerializer.h"
+#include "../inc/Article.h"
 
 using namespace depot::serialize;
 
 void DepotSerializer::serialize(std::ostream& out)
 {
   out << "Version: 1\n";
+  for (auto & article : depot::TopLevelArticles::getTopLevelArticles())
+  {
+    out << "Article\n";
+  }
 }
--- Test/src/depotTests/DepotSerializerTest.cpp
+++ Test/src/depotTests/DepotSerializerTest.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "../../../Code/depot/inc/DepotSerializer.h"
+#include "../../../Code/depot/inc/Article.h"
 #include <sstream>
 
 using namespace ::testing;
@@ -8,7 +9,12 @@ using namespace std::literals;
 struct DepotSerializerTest : public Test
 {
   depot::serialize::DepotSerializer serializer;
-  auto expected_output = "Version: 1\n"s;
+  std::string expected_output{"Version: 1\n"};
+
+  void TearDown() override
+  {
+    depot::TopLevelArticles::clearTopLevelArticles();
+  }
 };
 
 TEST_F(DepotSerializerTest, ShouldWriteVersionNumberWhenThereIsNoData)
@@ -20,5 +26,11 @@ TEST_F(DepotSerializerTest, ShouldWriteVersionNumberWhenThereIsNoData)
 
 TEST_F(DepotSerializerTest, ShouldWriteTopLevelArticle)
 {
+  std::ostringstream output;
 
+  auto article = depot::TopLevelArticles::createTopLevelArticle();
+  serializer.serialize(output);
+
+  expected_output += "Article\n";
+  EXPECT_EQ(expected_output, output.str());
 }
