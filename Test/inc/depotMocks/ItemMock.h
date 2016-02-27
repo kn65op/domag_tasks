24b7964cee4e1ce09150c3ea983018c5057beb2f
kn65op <kn65op@gmail.com>
2016-02-27 16:23:01 +0100

    Fix formatting for ItemMock

--- Test/inc/depotMocks/ItemMock.h
+++ Test/inc/depotMocks/ItemMock.h
@@ -3,25 +3,21 @@
 #include "depot/inc/Item.h"
 #include <gmock/gmock.h>
 
-namespace depot {
-namespace ut {
+namespace depot
+{
+namespace ut
+{
 
-class ItemMock : public IItem {
+class ItemMock : public IItem
+{
  public:
-  MOCK_METHOD3(buy,
-      void(double amount, double price, Date bdate));
-  MOCK_CONST_METHOD0(getQuantity,
-      double());
-  MOCK_CONST_METHOD0(getPricePerUnit,
-      double());
-  MOCK_METHOD2(consume,
-      void(double amount, Date date));
-  MOCK_CONST_METHOD0(getBuyDate,
-      boost::gregorian::date());
-  MOCK_CONST_METHOD0(getConsumeHistory,
-      ConsumeHistory::List());
-  MOCK_CONST_METHOD0(getThing,
-      std::shared_ptr<IArticle>());
+  MOCK_METHOD3(buy, void(double amount, double price, Date bdate));
+  MOCK_CONST_METHOD0(getQuantity, double());
+  MOCK_CONST_METHOD0(getPricePerUnit, double());
+  MOCK_METHOD2(consume, void(double amount, Date date));
+  MOCK_CONST_METHOD0(getBuyDate, boost::gregorian::date());
+  MOCK_CONST_METHOD0(getConsumeHistory, ConsumeHistory::List());
+  MOCK_CONST_METHOD0(getThing, std::shared_ptr<IArticle>());
   MOCK_CONST_METHOD0(getStorehauseImpl, std::shared_ptr<AbstractContainer>());
   MOCK_METHOD1(setStorehause, void(std::shared_ptr<AbstractContainer>));
 };
