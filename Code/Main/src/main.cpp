fc1b07a08e2155d75a9c254de5c085c75113a2cf
kn65op <kn65op@gmail.com>
2016-09-13 20:54:17 +0200

    Add logger to main

--- Code/Main/src/main.cpp
+++ Code/Main/src/main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <string>
+
+#include "TLogger.h"
+
+std::string getLogFileName()
+{
+  return "domag.log";
+}
 
 int main()
 {
-  std::cout << "domag\n";
+  TLogger::LoggerFacade logger(TLogger::LoggerType::FILE, TLogger::LogFileOnEntry::CREATE_WITH_DATE, TLogger::LogFileOnExit::REMOVE, getLogFileName());
 }
