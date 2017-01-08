862da95ff6d89802e7742278028f89bb3ffe2863
kn65op <kn65op@gmail.com>
2017-01-08 23:30:43 +0100

    Add compilation cli dirty

--- Code/Main/src/main.cpp
+++ Code/Main/src/main.cpp
@@ -1,4 +1,4 @@
-#include <string>
+/*#include <string>
 
 #include "../inc/DomagRunner.h"
 #include "TLogger.h"
@@ -10,9 +10,26 @@ std::string getLogFileName()
 
 int main()
 {
-  TLogger::LoggerFacade logger(TLogger::LoggerType::FILE, TLogger::LogFileOnEntry::OVERRIDE, TLogger::LogFileOnExit::DO_NOTHING, getLogFileName());
+  TLogger::LoggerFacade logger(TLogger::LoggerType::FILE, TLogger::LogFileOnEntry::OVERRIDE,
+TLogger::LogFileOnExit::DO_NOTHING, getLogFileName());
 
   LOG << "main";
 
   depot::main::DomagRunner runner;
+}
+*/
+
+#include "cli/common.h"
+#include "cli/console.h"
+#include "domagCli.h"
+
+int main(void)
+{
+  domag cli_EmptyCli;
+  cli::Shell cli_Shell(cli_EmptyCli);
+  cli::ResourceString welcome_message;
+  welcome_message.SetString(cli::ResourceString::LANG_EN, "Eloszka\n");
+  cli_Shell.SetWelcomeMessage(welcome_message);
+  cli::Console cli_Console(false);
+  cli_Shell.Run(cli_Console);
 }
