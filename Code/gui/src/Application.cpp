b562fcb265ea2fb437d0c11386a9d5cb3b495b14
kn65op <kn65op@gmail.com>
2017-08-09 20:26:47 +0200

    Little cleanup in Applciation

--- Code/gui/src/Application.cpp
+++ Code/gui/src/Application.cpp
@@ -10,7 +10,6 @@ namespace gui
 {
 
 std::unique_ptr<MainWindow> mainWindow;
-Gtk::Window* mainWindowGtk;
 
 void Application::prepareView()
 {
@@ -18,15 +17,9 @@ void Application::prepareView()
     view->refresh();
 }
 
-widget::NewContainerDialog* prepareDialog(MainWindow& window)
-{
-    return window.getNewContainerDialog();
-}
-
 void Application::openNewContainerDialog()
 {
-    const auto dialog = prepareDialog(*mainWindow);
-    //dialog->set_transient_for(*mainWindowGtk);
+    const auto dialog = mainWindow->getNewContainerDialog();
     dialog->setParentContainer();
     dialog->run();
     prepareView();
@@ -36,11 +29,10 @@ Application::Application()
 {
     auto app = Gtk::Application::create("org.domag");
     mainWindow = std::make_unique<MainWindow>();
-    mainWindowGtk = mainWindow->getWindow();
     prepareView();
     auto addContainerMenuItem = mainWindow->getAddTopLevelContainerMenuItem();
     addContainerMenuItem->signal_activate().connect(sigc::mem_fun(*this, &Application::openNewContainerDialog));
 
-    app->run(*mainWindowGtk);
+    app->run(*mainWindow->getWindow());
 }
 }
