f8e340fa1fb0be313b26a6c3d289cd3fa2651bbb
kn65op <199537+kn65op@users.noreply.github.com>
2019-03-27 19:24:28 +0100

    Deadline after creation is set date

--- android/app/src/test/java/com/example/domag/tasks/RecurringTaskTest.kt
+++ android/app/src/test/java/com/example/domag/tasks/RecurringTaskTest.kt
@@ -3,20 +3,37 @@ package com.example.domag.tasks
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.Test
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class RecurringTaskTest {
     private val summary = "Summary"
+    private val date1 = ZonedDateTime.of(
+        2011,
+        12,
+        3,
+        10,
+        15,
+        30,
+        0,
+        ZoneId.of("+01:00")
+    )
+    private val task = RecurringTask(summary, date1)
 
-    private val task = RecurringTask(summary)
     @Test
     fun `should has proper type`() {
         assertThat(task.type, equalTo("RECURRING TASK"))
     }
 
     @Test
-    fun `should always be not done` () {
+    fun `should always be not done`() {
         val notDone = false
 
         assertThat(task.done, equalTo(notDone))
+    }
+
+    @Test
+    fun `nextDeadline should be date of first deadline`() {
+        assertThat(task.nextDeadline, equalTo(date1))
     }
 }
