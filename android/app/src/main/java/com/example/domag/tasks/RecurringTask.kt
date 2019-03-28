867ce7b9e9b20830dfc77cca0dd80c7c45968fc6
Tomasz Drzewiecki <199537+kn65op@users.noreply.github.com>
2019-03-28 19:25:53 +0100

    Add recurring task

---
+++ android/app/src/main/java/com/example/domag/tasks/RecurringTask.kt
@@ -0,0 +1,28 @@
+package com.example.domag.tasks
+
+import java.time.Period
+import java.time.ZonedDateTime
+
+class RecurringTask(
+    override var summary: String,
+    firstDeadline: ZonedDateTime = ZonedDateTime.now(),
+    private val period: Period,
+    override var id: Id = 0
+) : Task {
+    companion object {
+        const val type = "RECURRING TASK"
+    }
+
+    override var nextDeadline: ZonedDateTime = firstDeadline
+    override val type
+        get() = Companion.type
+    override var done: Boolean
+        get() = false
+        set(value) {
+            if (value) nextDeadline = ZonedDateTime.now().plus(period)
+        }
+
+    override fun serializeToString(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
