71995929e24b5f386b2c7302d9bfa8e35c8edbe1
kn65op <199537+kn65op@users.noreply.github.com>
2019-08-15 20:38:54 +0200

    Add  period

---
+++ android/app/src/main/java/com/example/domag/utils/time/Period.kt
@@ -0,0 +1,3 @@
+package com.example.domag.utils.time
+
+cl
---
+++ android/app/src/test/java/com/example/domag/utils/time/PeriodTest.kt
@@ -0,0 +1,10 @@
+package com.example.domag.utils.time
+
+import java.time.Period
+
+class PeriodTest {
+    @Test
+    fun `shuold be created`()
+    {
+    }
+}
