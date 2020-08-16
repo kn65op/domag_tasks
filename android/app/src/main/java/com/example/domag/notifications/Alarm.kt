ef32cb694d5d12e867f2d00d713748f19dedaeb5
kn65op <199537+kn65op@users.noreply.github.com>
2020-08-16 10:45:10 +0200

    Test

--- android/app/src/main/java/com/example/domag/notifications/Alarm.kt
+++ android/app/src/main/java/com/example/domag/notifications/Alarm.kt
@@ -37,7 +37,6 @@ class Alarm {
         set(Calendar.HOUR_OF_DAY, 8)
         set(Calendar.MINUTE, 0)
         set(Calendar.SECOND, 0)
-
     }
 
     private fun alarmIntent(context: Context?): PendingIntent =
