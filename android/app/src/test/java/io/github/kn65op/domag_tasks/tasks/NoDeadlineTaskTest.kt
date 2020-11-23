6bcefac5e6f933cfc04850b4200e5c2a15ed5dd5
kn65op <199537+kn65op@users.noreply.github.com>
2020-11-23 18:47:51 +0100

    Add test

--- android/app/src/test/java/io/github/kn65op/domag_tasks/tasks/NoDeadlineTaskTest.kt
+++ android/app/src/test/java/io/github/kn65op/domag_tasks/tasks/NoDeadlineTaskTest.kt
@@ -1,10 +1,17 @@
 package io.github.kn65op.domag_tasks.tasks
 
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import org.junit.Test
 
 class NoDeadlineTaskTest {
+    val summary = "Some task"
+    val task = NoDeadlineTask(summary)
+
     @Test
-    fun `should Create task`() {
-        NoDeadlineTask("Some task")
+    fun `serializeToString should serialize to Json`() {
+        val expectedText =
+            """{"summary":"$summary", "id":0}"""
+        assertThat(task.serializeToString(), equalTo(expectedText))
     }
 }
